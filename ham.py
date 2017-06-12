faaa18d745e03864e177155416334b32f9ff61a9
ankita1236 <ankita1236@gmail.com>
2017-06-12 12:03:50 +0530

    classifier naive bayes

--- ham.py
+++ ham.py
@@ -7,15 +7,15 @@ import pyPdf
 
 d={}
 i=1
-path="/home/ankita/Screen-Resume/allresumes"
+path="/home/ankita/Screen-Resume/allresumes/ham"
 
 for filename in os.listdir(path):
-	pdf = pyPdf.PdfFileReader(open("/home/ankita/Screen-Resume/allresumes/"+filename, "rb"))
-	key=str(i)+".txt"
+	pdf = pyPdf.PdfFileReader(open("/home/ankita/Screen-Resume/allresumes/ham/"+filename, "rb"))
+	key='h'+str(i)+".txt"
 	value=filename
 	d[key]=value
-	f=open(str(i)+".txt","w")
-	a=open("allresumes.txt","a")
+	f=open('h'+str(i)+".txt","w")
+	a=open("allhamresumes.txt","a")
 	for page in pdf.pages:
 		f.write(page.extractText())
 		a.write(page.extractText())
